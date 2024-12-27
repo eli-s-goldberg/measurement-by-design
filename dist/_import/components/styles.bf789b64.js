6857f568a1b94856f5397456651361a1be52dd69
Eli Goldberg <eligoldberg@Mac.localdomain>
2024-12-26 22:10:37 -0500

    updated dist build

--- dist/_import/components/styles.bf789b64.js
+++ dist/_import/components/styles.bf789b64.js
@@ -1,5 +1,369 @@
 // styles.js
 
+export const pwcStyle = `
+ /* Sidebar Styles */
+  #observablehq-sidebar {
+    --observablehq-sidebar-padding-left: calc(max(0rem, (100vw - var(--observablehq-max-width)) / 2));
+    position: fixed;
+    background: black;
+    color: white;
+    font: 14px var(--sans-serif);
+    visibility: hidden;
+    font-weight: 500;
+    width: calc(272px + var(--observablehq-sidebar-padding-left));
+    z-index: 3;
+    top: 0;
+    bottom: 0;
+    left: -272px;
+    box-sizing: border-box;
+    padding: 0 0.5rem 1rem calc(var(--observablehq-sidebar-padding-left) + 0.5rem);
+    overflow-y: auto;
+  }
+
+  #observablehq-sidebar > ol:first-child::before {
+    background: #2b2b2b;
+  }
+
+  #observablehq-sidebar summary {
+    font-weight: 700;
+    color: white;
+    background: black;
+    cursor: default;
+  }
+
+  #observablehq-sidebar details summary {
+    background: black;
+  }
+
+  #observablehq-sidebar details summary:hover {
+    background: #D93954;
+  }
+
+  /* Link Styles */
+  .observablehq-link a:hover {
+    background: #FFB600;
+  }
+
+  .observablehq-link-active a {
+    background: black;
+  }
+
+  .observablehq-link-active::before {
+    background: #e64646;
+  }
+
+  .observablehq-link-active a {
+    color: #ffffff;
+    background: black;
+  }
+
+  .observablehq-link:not(.observablehq-link-active) a[href]:not(:hover) {
+    color: #cccccc;
+  }
+
+  /* Search Styles */
+  #observablehq-search input {
+    background-color: #3d3d3d;
+    color: #ffffff;
+  }
+
+  #observablehq-search input::placeholder {
+    color: #808080;
+  }
+
+  /* General Sidebar Styles */
+  #observablehq-sidebar > ol,
+  #observablehq-sidebar > details,
+  #observablehq-sidebar > section {
+    border-bottom-color: #404040;
+  }
+
+  /* Page Title Styles */
+  .page-title {
+    font-family: "ITC Charter Com", serif;
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 52px;
+    letter-spacing: -0.03em;
+  }
+  .table-header-style {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 24.42px;
+  }
+
+  .page-title-header {
+    font: 18px/1.5 "Helvetica Neue", Arial, sans-serif;
+    margin: 0;
+    max-width: 100%;
+    letter-spacing: -0.03em;
+    margin-bottom: 100px; 
+  }
+
+  /* Card Styles */
+  .card {
+    background-color: white; 
+    border-radius: 0.5rem;
+    padding: 1.5rem;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: left;
+  }
+
+  .card-yellow {
+    background-color: #ffb600;
+    color: #111827;
+  }
+
+  .card-white {
+    background-color: #ffffff;
+  }
+
+  .card-gray {
+    background-color: #ffffff;
+  }
+
+  /* Text Styles */
+  .text-header {
+    font: 13px/1.5 "Helvetica Neue", Arial, sans-serif;
+    line-height: 1rem;
+    font-weight: 600;
+    color: #000000;
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
+    margin-bottom: 0.25rem;
+  }
+
+  .text-title {
+    font: 52px/1.5 "ITC Charter Com", serif;
+    line-height: 2rem;
+    color: #111827;
+    margin-bottom: 0.5rem;
+    letter-spacing: 0.02em;
+  }
+
+  .text-subtext {
+    font: 15.4px/1.5 "Helvetica Neue", Arial, sans-serif;
+    line-height: 1.25rem;
+    color: #000000;
+    font-weight: 600;
+    margin-top: 1rem;
+  }
+
+  .text-subheader {
+    font-family: 'Georgia', serif;
+    font-weight: 400;
+    font-size: 38px;
+    line-height: 43.18px;
+    letter-spacing: -3%;
+    margin-bottom: 1em;
+}
+    .text-subheader-pullquote {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 31px;
+}
+
+.text-subheader-pullquote-highlight {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 31px;
+    background-color: #ffb600; /* Adds the background */
+    color: black; /* Ensures text color contrasts well */
+}
+
+  /* Grid Layout */
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1.5rem;
+  }
+  .grid-vertical-stack {
+    display: grid;
+    grid-template-rows: auto auto; /* Two rows */
+    gap: 1.5rem; /* Spacing between cards */
+    width: 100%; /* Full width */
+    padding: 1rem; /* Padding for the stack */
+}
+
+.grid-card {
+    background-color: #ffb600; /* Card background color */
+    border-radius: 10px; /* Rounded corners */
+    padding: 1.5rem; /* Inner padding */
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* Shadow for a clean look */
+    display: flex;
+    flex-direction: column; /* Stack content vertically */
+    gap: 0.5rem; /* Spacing between content */
+}
+
+.grid-title {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 700;
+    font-size: 16px;
+    margin: 0;
+    text-transform: uppercase;
+}
+
+.divider {
+    width: 40px;
+    border: 1px solid black;
+    margin: 0.5rem 0;
+}
+
+.grid-value {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 700;
+    font-size: 32px;
+    margin: 0;
+}
+
+.grid-subtitle {
+    font-family: 'Helvetica Neue', serif;
+    font-weight: 400;
+    font-size: 18px;
+    margin: 0;
+}
+
+.grid-description {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    margin: 0;
+    color: #333; /* Slightly darker text for readability */
+}
+
+  .pull-quote-grid-card {
+    display: grid;
+    grid-template-columns: 2fr 1fr; /* Left side 2/3, Right side 1/3 */
+    grid-template-rows: auto auto; /* Two rows */
+    gap: 10px; /* Spacing between items */
+    width: 100%; /* Adjust width as needed */
+    background-color: #F2F2F2; /* Background for the card */
+    padding: 10px; /* Add padding */
+}
+
+.pull-quote-grid-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #F2F2F2;
+    border: 0px solid black;
+    font-weight: bold;
+}
+
+.pull-quote-item-a {
+    grid-column: 1; /* First column */
+    grid-row: 1; /* First row */
+    height: 100%; /* Adjust height as needed */
+}
+
+.pull-quote-item-b {
+    grid-column: 2; /* Second column */
+    grid-row: 1 / span 2; /* Spans two rows */
+    height: 100%; /* Combined height of A and C */
+}
+
+.pull-quote-item-c {
+    grid-column: 1; /* First column */
+    grid-row: 2; /* Second row */
+    height: 100%; /* Adjust height as needed */
+}
+
+
+  /* Global Styles */
+  html {
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: white;
+    color: var(--theme-foreground);
+  }
+
+  body {
+    max-width: var(--observablehq-max-width);
+    margin: auto;
+    overflow-x: hidden; /* Prevent horizontal scroll */
+  }
+
+  /* Full Width Div */
+  .full-width-div {
+    /* Span the entire viewport width */
+    width: 80vw;
+    /* Position relative to allow offset */
+    position: relative;
+    /* Offset the div to the left by 50% of the viewport width */
+    left: 50%;
+    /* Shift the div back by 50% of its own width */
+    transform: translateX(-50%);
+    /* Light gray background */
+    background-color: #d3d3d3; /* You can also use 'lightgray' */
+    /* Padding before the text starts */
+    padding: 0px; /* Adjust as needed */
+    /* Optional: Add box-sizing for consistent sizing */
+    box-sizing: border-box;
+    /* Optional: Remove any margins */
+    margin: 10px;
+  }
+
+  /* Table Styles */
+  table {
+    background-color: white;
+    width: 100%;
+    border-collapse: collapse;
+    font: 13px/1.2 var(--serif);
+    font-family: "ITC Charter Com", serif;
+    letter-spacing: -0.03em;
+  }
+
+  th {
+    color: black;
+    background-color: white;
+    letter-spacing: -0.03em;
+  }
+
+  td {
+    color: var(--theme-foreground-alt);
+    vertical-align: top;
+  }
+
+  /* Full Width Section */
+  .full-width-section {
+    position: relative;
+    background-color: transparent; /* No background on the main div */
+    padding: 1.5rem 0; /* Vertical padding */
+  }
+
+  .full-width-section::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 100vw;
+    height: 100%;
+    background-color: #F2F2F2;
+    transform: translateX(-50%);
+    z-index: -1;
+  }
+
+  .full-width-section .content-container {
+    max-width: var(--observablehq-max-width);
+    margin: 0 auto;
+    padding: 0 0rem;
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 768px) {
+    .full-width-section .content-container {
+      padding: 0 1rem;
+    }
+  }
+`;
+
 export const defaultStyles = `
 
   .variable {
@@ -78,13 +442,6 @@ export const defaultStyles = `
     border-collapse: collapse;
     padding: 2px;
     font-size: 0.8em;
-  }
-
-  .horizontal-line {
-    border-top: 0.5px solid #d3d3d3;
-    width: 100%;
-    margin-top: 10px;
-    margin-bottom: 10px;
   }
 
   .katex { font-size: 1em; }
